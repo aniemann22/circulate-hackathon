085bdae1580148dac339a3fd792d557502242bbf
Ash Niemann <68298163+aniemann22@users.noreply.github.com>
2024-10-19 14:49:04 -0400

    Set up routes for navigating between pages

--- circulate/src/App.js
+++ circulate/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.tsx';
+import AboutUs from './pages/AboutUs.tsx';
+import Products from './pages/Products.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/Products" element={<Products />} />
+      </Routes>
+    </Router>
   );
 }
 
