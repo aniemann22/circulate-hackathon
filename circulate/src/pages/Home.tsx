fd457f6b255c2169f7da028f5cc37c6c25409607
Matthew Manganillo <matthewmanganillo@dyn233072.cc.lehigh.edu>
2024-10-19 15:21:05 -0400

    added footer and applied to all pages, added all required artifacts

--- circulate/src/pages/Home.tsx
+++ circulate/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
 
 const Home: React.FC = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 };
